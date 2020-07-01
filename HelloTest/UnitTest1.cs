81eced59218f0994beafddc7b4e8bc9ce23bb9d2
adel.sattari <adel.sattari@SIMPLET>
2020-07-01 14:14:37 +0200

    ajout du code source

---
+++ HelloTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello;
+
+
+namespace HelloTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World",Program.createMessage());
+        }
+    }
+}
